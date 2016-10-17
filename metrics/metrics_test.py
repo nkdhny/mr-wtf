dbc061a5c312f015b6513fcfcb3947ccb1290bbe
Alexey Golomedov <golomedov@gmail.com>
2016-10-17 19:19:25 +0300

    emmit only 200 codes

---
+++ metrics/metrics_test.py
@@ -0,0 +1,16 @@
+import unittest
+from metrics import *
+
+class TestMetrics(unittest.TestCase):
+
+    def test_should_parse_code(self):
+        l = '195.225.111.113 - - [13/Oct/2016:00:00:00 +0400] "GET /id45037 HTTP/1.1" 200 26236 "-" "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.99 Safari/537.36"'
+        rec = parse_line(l)
+        self.assertEqual(rec['code'], 200)
+
+        l = '195.225.111.113 - - [13/Oct/2016:00:00:00 +0400] "GET /id45037 HTTP/1.1" 404 26236 "-" "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.99 Safari/537.36"'
+        rec = parse_line(l)
+        self.assertEqual(rec['code'], 404)
+
+if __name__ == '__main__':
+    unittest.main()
