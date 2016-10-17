469bd06a3b8b192b47c67ba620f28166329267cc
Alexey Golomedov <golomedov@gmail.com>
2016-10-17 19:02:13 +0300

    luigi config

---
+++ metrics/metrics.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import luigi
+import luigi.contrib.hadoop
+
+class LogFile(luigi.ExternalTask):    
+    date = luigi.DateParameter()
+
+    def output(self):
+        return luigi.contrib.hdfs.HdfsTarget(self.date.strftime('/user/sandello/logs/access.log.%Y-%m-%d'))
+
+class TotalHitsTask(luigi.contrib.hadoop.JobTask):
+    date_interval = luigi.DateIntervalParameter()
+
+    def output(self):
+        return luigi.contrib.hdfs.HdfsTarget("/user/agolomedov/total_hits_{}".format(self.date_interval))
+
+    def requires(self):
+        return [LogFile(date) for date in self.date_interval]
+
+    def mapper(self, line):
+        yield "total_hits", 1
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+    combiner = reducer
