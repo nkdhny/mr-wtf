143db5e3b9d44e0f6966eb9234a810d4c4385919
Alexey Golomedov <golomedov@gmail.com>
2016-10-21 15:58:02 +0300

    typo

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class AppConfig(object):
     metrics_path='/home/agolomedov/hw1/metrics'
-    streaming_root='/home/agolomedov/hw1/mr_wtf/streaming/'
+    streaming_root='/home/agolomedov/hw1/mr-wtf/streaming/'
