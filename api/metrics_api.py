c2e12532225812a13e5aa91e864a646e986f8b06
Alexey Golomedov <golomedov@gmail.com>
2016-11-21 13:35:49 +0300

    +expose profile hits through api

--- api/metrics_api.py
+++ api/metrics_api.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import jsonify
 from api import app
 from flask import request
@@ -6,6 +8,7 @@ from metrics.local import LocalTotalHitsMetric, LocalTotalUsersMetric, LocalUser
 import datetime
 import logging
 from flask import abort
+import happybase
 
 class MetricNotReady(Exception):
     pass
@@ -70,3 +73,38 @@ def get_metrics():
 
 
     return jsonify(metrics)
+
+#HW-2
+def _compose_table_name(metric):
+    return "bigdatashad_{}_{}".format("agolomedov", metric)
+
+def _hbase_connection():
+
+    def choose_hbase_host():
+        HOSTS = ["hadoop2-%02d.yandex.ru" % i for i in xrange(11, 14)]
+        return random.choice(HOSTS)
+
+    return happybase.Connection(choose_hbase_host())
+
+
+@app.route('/api/hw2/profile_hits')
+def get_profile_hits():
+
+    start_date = datetime.datetime.strptime(request.args.get("start_date"), "%Y-%m-%d").date()
+    end_date = datetime.datetime.strptime(request.args.get("end_date"), "%Y-%m-%d").date()
+    profile_id = int(request.args.get('profile_id')[2:])
+
+    all_hits = _hbase_connection().table(_compose_table_name("profile_hits"))
+
+    def compose_key(profile, day):
+        return b"{}#{}".format(profile, day.strptime("%Y-%m-%d"))
+
+    mathched_hits = all_hits.scan(
+            row_start=compose_key(profile_id, start_date), row_stop=compose_key(profile_id, end_date))
+
+    def parse_key(key):
+        return key.split('#')[1]
+
+    return jsonify(dict([
+        (parse_key(k), [d.get(x, 0) for x in range(25)]) for k, d in mathched_hits
+    ]))
