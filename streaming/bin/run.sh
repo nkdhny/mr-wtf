e9f37b1df8e3afddeeddceb21a0ba64671e8bc27
Alexey Golomedov <golomedov@gmail.com>
2016-12-05 18:21:48 +0300

    + bin

--- streaming/bin/run.sh
+++ streaming/bin/run.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+cd ../src
+spark-submit \
+      --jars /opt/cloudera/parcels/CDH/jars/spark-streaming-kafka_2.10-1.6.0-cdh5.9.0.jar \
+      --master yarn-client \
+      --num-executors 2 \
+      --executor-cores 1 \
+      --executor-memory 2048m \
+      --conf 'appName=NkdhnyErrorsCount' app.py
